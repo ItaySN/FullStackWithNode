a964574848d4c171ace2789e31c12a65438bbc6e
ItaySN <itayfic@gmail.com>
2020-09-01 00:38:29 +0300

    EX 3.1 3.2

--- backend/index.js
+++ backend/index.js
@@ -1,86 +1,67 @@
 const http = require('http');
 const express = require('express');
-const { response, json } = require('express');
-const app = express();
+const { res, json } = require('express');
+const { brotliDecompress } = require('zlib');
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
+
+const app = express();
+app.use(express.json());
+
+
+let persons = [
+{
+    name:"Arto Hellas",
+    number: "040=123456",
+    id:1
+},
+{
+    name:"Dan Avramov",
+    number: "39-44-5323523",
+    id:2
+},
+{
+    name:"Mary Poppendieck",
+    number: "39-23-6423122",
+    id:3
+},
+];
+  
 
 app.get('/', (req,res) =>{
     res.send('<h1>Hello World! </h1>');
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
-app.get('/api/notes/:id', (req,res) => {
+app.get('/info', (req,res) =>{
+    if(persons.length === 0)
+    {
+        res.send(`<p> The phonebook is empty </p> <br> ${new Date()}`);
+    }
+    res.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`);
+})
+
     const id = Number(req.params.id);
-    const note = notes.find(note => note.id === id)
-    if (note) {
-    res.json(note)
   } else {
     res.status(404).end()
   }
 }) 
 
-app.delete('api/notes/:id', (req,res) => {
-    const id = req.params.id;
-    notes = notes.filter(note => note.id!==id)
 
     res.status(204).end();
 })
 
 
-const generateId = () => {
-    const maxId = notes.length > 0 
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-    return maxId + 1 ;
 }
 
-app.post('/api/notes', (req,res) => {
-    const body = req.body;
 
-    if(!body.content){
-        return response.status(400).json({
-            error: 'content missing'
-        })
     }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
     }
 
-    notes = notes.concat(note);
-    res.json(note);
-})
 
-// const app = http.createServer((request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'application/json' })
-//   response.end(JSON.stringify(notes))
-// })
 
 const PORT = 3001;
 app.listen(PORT);
