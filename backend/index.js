3e0adb1e4f9a14f4bfa3a3aef8a3a65fd0f964c1
ItaySN <itayfic@gmail.com>
2020-08-31 04:29:44 +0300

    functions by instructions
    write the GET , POST , DELETE methods for the server .

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const { response, json } = require('express');
 const app = express();
 
 let notes = [
@@ -29,6 +30,51 @@ app.get('/', (req,res) =>{
 
 app.get('/api/notes', (req, res) => {
   res.json(notes)
+})
+
+app.get('/api/notes/:id', (req,res) => {
+    const id = Number(req.params.id);
+    const note = notes.find(note => note.id === id)
+    if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+}) 
+
+app.delete('api/notes/:id', (req,res) => {
+    const id = req.params.id;
+    notes = notes.filter(note => note.id!==id)
+
+    res.status(204).end();
+})
+
+
+const generateId = () => {
+    const maxId = notes.length > 0 
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+    return maxId + 1 ;
+}
+
+app.post('/api/notes', (req,res) => {
+    const body = req.body;
+
+    if(!body.content){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note);
+    res.json(note);
 })
 
 // const app = http.createServer((request, response) => {
