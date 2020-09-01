f4bba59f8bbfab57064a73159388771cb57e259d
ItaySN <itayfic@gmail.com>
2020-09-01 21:34:20 +0300

    Functions
    created functions to create a schema for person and create a model for person with the person schema .
    In addition I create two functions for display the list of the DB and adding new person for the DB

--- mongo.js
+++ mongo.js
@@ -1,32 +1,42 @@
 const mongoose = require('mongoose')
 
+
 if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
-
-const password = process.argv[2]
 
 const url =
  `mongodb+srv://itayFullS:gkXEHjPspPvlt29i@cluster0.0ak1s.mongodb.net/personDB?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+
+const name = process.argv[3];
+const number = process.argv[4];
+if(name && number){
+    const person = new Person({
+    name:   name,
+    number: number,
 })
-
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
+person.save().then(result => {
+console.log('person saved!');
+mongoose.connection.close();
 })
-
-note.save().then(result => {
-  console.log('note saved!')
+}else{
+    Person.find({}).then(result => {
+    result.forEach(person => {
+    console.log(person);
+  })
   mongoose.connection.close()
 })
+};
+
+
